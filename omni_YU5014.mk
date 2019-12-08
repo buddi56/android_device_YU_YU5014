2429ea84a114b64c112e5308c156d428068aa569
buddi56 <ynaveenk.0513@gmail.com>
2019-12-08 23:16:04 +0530

    removed some unwanted data, flags. Added wipe vendor

--- omni_YU5014.mk
+++ omni_YU5014.mk
@@ -2,7 +2,6 @@
 PRODUCT_RELEASE_NAME := YU5014
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit from our custom product configuration
@@ -14,14 +13,3 @@ PRODUCT_DEVICE := YU5014
 PRODUCT_MANUFACTURER := YU
 PRODUCT_MODEL := YU5014
 PRODUCT_NAME := omni_YU5014
-
-# enable stock zip packages flash
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.treble.enabled=true \
-    persist.sys.usb.config=mtp,adb \
-    persist.service.adb.enable=1 \
-    persist.service.debuggable=1 \
-    ro.secure=1 \
-    ro.adb.secure=0 \
-    ro.debuggable=1 \
-    ro.allow.mock.location=0
